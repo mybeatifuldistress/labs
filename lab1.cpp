60172d8cb0fdcad66957a256496294782f47f45b
Danila <dankkk11@gmail.com>
2024-09-05 19:27:48 +0300

    Sum in row and col

--- lab1.cpp
+++ lab1.cpp
@@ -1,43 +1,51 @@
 ﻿#include <iostream>
-#include <limits>
+#include <ctime>
 
 int main() {
 
-	int min = INT_MAX;
-	int max = INT_MIN;
+	srand(time(NULL));
 
-	int size;
+	int array[100][100];
+	int rows;
+	int cols;
 
-	std::cout << "Enter array size: " << std::endl;
-	std::cin >> size;
+	std::cout << "Enter the number of rows: " << std::endl;
+	std::cin >> rows;
+	
+	std::cout << "Enter the number of cols: " << std::endl;
+	std::cin >> cols;
 
-	int* array = new int[size];
-
-	std::cout << "Enter numbers of array: " << std::endl;
-	for (int i = 0; i < size; ++i) {
-		std::cin >> array[i];
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			array[i][j] = 1 + rand() % 9;
+		}
 	}
 
-	std::cout << "Array values: ";
-	for (int i = 0; i < size; ++i) {
-		std::cout << array[i] << ' ';
+	std::cout << "Array:" << std::endl;
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			std::cout << array[i][j] << ' ';
+		}
+		std::cout << std::endl;
 	}
+	
+	for (int i = 0; i < rows; ++i) {
+		int sum_row = 0;
+		for (int j = 0; j < cols; ++j) {
+			sum_row += array[i][j];
+		}
+		std::cout << "Amount in row " << i + 1 << ": " << sum_row << std::endl;
+	}
+
 	std::cout << std::endl;
 
-	for (int i = 0; i < size; ++i) {
-		if (array[i] < min) {
-			min = array[i];
+	for (int j = 0; j < cols; ++j) {
+		int sum_col = 0;
+		for (int i = 0; i < rows; ++i) {
+			sum_col += array[i][j];
 		}
-		if (array[i] > max) {
-			max = array[i];
-		}
+		std::cout << "Amount in col " << j + 1 << ": " << sum_col << std::endl;
 	}
-
-	std::cout << "Min is " << min << std::endl;
-	std::cout << "Max is " << max << std::endl;
-	std::cout << "Difference between max and min: " << max - min << std::endl;
-
-	delete[] array;
 
 	return 0;
 }
